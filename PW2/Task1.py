abd4c8b82f3fb4fecfd9fda043a1b7ea10ef446e
ShepelM1 <143932992+ShepelM1@users.noreply.github.com>
2024-02-15 12:40:31 +0200

    Update Task1.py

--- PW2/Task1.py
+++ PW2/Task1.py
@@ -2,7 +2,7 @@ my_list = [-1, -2, -3, 0, 1, 2, 3, 4, 5]
 suma = 0
 
 for i in my_list:
-    if my_list[i]>0:
-        suma+=my_list[i]
+    if i>0:
+        suma+=i
 
-print(suma)
+print(suma/len(my_list))
